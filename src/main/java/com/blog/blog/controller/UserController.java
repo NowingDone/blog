68e19eb06eb896a387679f633c9aab7e4a8f3a36
e <zhangxiaoqing@hckuku.com>
2018-09-11 09:56:42 +0800

    新建类

--- src/main/java/com/blog/blog/controller/UserController.java
+++ src/main/java/com/blog/blog/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.blog.blog.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @ApiOperation(value="获取用户信息",notes = "根据用户id获取用户信息")
+    @RequestMapping("/user")
+    public String getInfo(){
+
+        return "nihao1";
+
+    }
 }
